6b1ea4948b14b83c97b6255d0b6fea54fad7aaaf
Adhisikha Patnaik <56517351+adhi-2311@users.noreply.github.com>
2020-10-10 01:23:49 +0530

    Create subarray_sum_equals_k.java

---
+++ Arrays/subarray_sum_equals_k.java
@@ -0,0 +1,24 @@
+/*
+Given an array of integers and an integer k, we need to find the total number of continuous subarrays whose sum equals to k.
+*/
+class Solution {
+    public int subarraySum(int[] arr, int x) {
+        
+     int ans=0;
+     long curr_sum=0l;
+     HashMap<Long,Long> prev=new HashMap<Long,Long>(); //Hashing for storing the number of times curr_sum occurs 
+     prev.put(0l,1l);
+     for(int i=0;i<arr.length;i++)
+     {
+      curr_sum+=arr[i];
+      if(prev.containsKey(curr_sum-x))
+        ans+=prev.get(curr_sum-x);
+
+      if(!prev.containsKey(curr_sum))
+        prev.put(curr_sum,1l);//new curr_sum
+      else
+        prev.put(curr_sum,prev.get(curr_sum)+1);
+     }
+    return ans;
+    }
+}
