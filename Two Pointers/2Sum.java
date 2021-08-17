b8952babd802a25aaf72e0496c79ace7650f2c2e
Adhisikha Patnaik <56517351+adhi-2311@users.noreply.github.com>
2021-08-17 11:52:57 +0530

    Update 2Sum.java

--- Two Pointers/2Sum.java
+++ Two Pointers/2Sum.java
@@ -1,29 +1,68 @@
-/* Given a sorted array, find two numbers that add up target value.
-function should return the indices of the two numbers stored in an array such that they add up to the target.
-same element must not be used twice
-indices must be distinct index1<index2 and follow 1-based indexing
-*/
 
+
+//nlogn method
 class Solution {
-    public int[] twoSum(int[] arr, int target) {
-        int l=0;
-        int h=arr.length-1;
-        int ans[]=new int[2];
-        Arrays.fill(ans,0);
-        while(l<=h)
+    class Pair{
+        int ele,idx;
+        Pair(int i,int e)
         {
-          if((arr[l]+arr[h])==target)
+            ele=e;
+            idx=i;
+        }
+    }
+    public int[] twoSum(int[] nums, int target) {
+        
+        int l=0;
+        int h=nums.length-1;
+        Pair arr[]=new Pair[h+1];
+        int ans[]=new int[2];
+        for(int i=0;i<=h;i++)
+            arr[i]=new Pair(i,nums[i]);
+        Arrays.sort(arr,(a,b)->(a.ele-b.ele));
+        while(l<h)
+        {
+          if((arr[l].ele+arr[h].ele)==target)
           {
-              ans[0]=l+1;
-              ans[1]=h+1;
+              ans[0]=arr[l].idx;
+              ans[1]=arr[h].idx;
               break;
           }
-            else if((arr[l]+arr[h])>target)
+            else if((arr[l].ele+arr[h].ele)>target)
                 h-=1;
             else
                 l+=1;
         }
         return ans;
+    }
+}
+
+// O(N)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int n=nums.length;
+        if(n<2)
+            return new int[2];
+        int ans[]=new int[2];
+        HashMap<Integer,Integer> hm=new LinkedHashMap<Integer,Integer>();
+        
+        
+        //a+b=target
+        for(int i=0;i<n;i++)
+        {
+            int a=nums[i];
+            if(hm.containsKey(target-a)==true)
+            {
+                    ans[0]=hm.get(target-a);
+                    ans[1]=i;
+                    break;
+            }
+            else hm.put(nums[i],i);
+        }
+        return ans;
         
     }
 }
+
+
+
