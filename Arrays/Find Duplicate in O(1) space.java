35376eedf6913720d99244084032b97922dad989
Adhisikha Patnaik <56517351+adhi-2311@users.noreply.github.com>
2021-09-16 01:02:09 +0530

    Create Find Duplicate in O(1) space.java

---
+++ Arrays/Find Duplicate in O(1) space.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            int curr=Math.abs(nums[i]);
+            if(nums[curr]<0)
+                return curr;
+            
+            nums[curr]*=(-1);
+        }
+         return -1;
+    }
+   
+}
