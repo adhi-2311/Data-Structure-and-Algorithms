cd47ae0f31ebcc477544cf36ec9255209b7e2f97
Adhisikha Patnaik <56517351+adhi-2311@users.noreply.github.com>
2022-02-14 01:51:22 +0530

    Update Path Compression.java

--- Disjoint Set Unions/Path Compression.java
+++ Disjoint Set Unions/Path Compression.java
@@ -1,4 +1,10 @@
+/*
+to find the root node, we need to traverse the parent nodes sequentially until we reach the root node. 
+If we search the root node of the same element again, we repeat the same operations. 
+After finding the root node, we can update the parent node of all traversed elements to their root node. 
+When we search for the root node of the same element again, we only need to traverse two elements to find its root node, which is highly efficient. 
 
+*/
 public int find(int x) {
         if(x==parent[x]) 
         return x;
@@ -9,3 +15,5 @@ public int find(int x) {
 }
 
 //O(LOGN)
+
+
