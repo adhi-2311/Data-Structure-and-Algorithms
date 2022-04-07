bdf5193bc5459b6d07ed11d67cc5d7ccb57e3b8a
Adhisikha Patnaik <56517351+adhi-2311@users.noreply.github.com>
2022-04-07 19:12:39 +0530

    Create Group Anagrams[CCI].java

---
+++ Arrays/Group Anagrams[CCI].java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/group-anagrams/
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map=new HashMap<>();
+        
+        for(int i=0;i<strs.length;i++){
+            String s1=strs[i];
+            char[] arr=s1.toCharArray();
+            Arrays.sort(arr);
+            String str=new String(arr);
+            
+            if(!map.containsKey(str)){
+                map.put(str,new ArrayList<>());
+            }
+            
+            map.get(str).add(s1);
+            
+            
+        }
+        return new ArrayList<>(map.values());
+    }
+}
