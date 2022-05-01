bf081b9b1ad3729d75b268298f699d22705afc1e
Adhisikha Patnaik <56517351+adhi-2311@users.noreply.github.com>
2022-05-01 18:46:36 +0530

    Update Subset Sum.java

--- Dynamic Programming/Subset Sum.java
+++ Dynamic Programming/Subset Sum.java
@@ -17,6 +17,61 @@ f(i,j)=f(i-1,j) || f(i-1,j-arr[i-1])
 */
 
 class Solution{
+    public static int recursive(int nums[],int idx,int target){
+        if(target==0)
+        return 1;
+        if(idx==0)
+        return (nums[idx]==target)?1:0;
+		  
+        int exclude=recursive(nums,idx-1,target);
+        int include=0;
+		  
+        if(nums[idx]<=target)
+        include=recursive(nums,idx-1,target-nums[idx]);
+
+        return exclude|include;
+    }
+    
+	public static int memoized(int nums[],int idx,int target,int dp[][]){
+        if(target==0)
+        return 1;
+        if(idx==0)
+        return (nums[idx]==target)?1:0;
+		
+		if(dp[idx][target]!=-1)
+			return (dp[idx][target]==1)?1:0;
+		
+        int exclude=memoized(nums,idx-1,target,dp);
+        int include=0;
+        if(nums[idx]<=target)
+        include=memoized(nums,idx-1,target-nums[idx],dp);
+
+        dp[idx][target]=exclude|include;
+		
+		return dp[idx][target];
+    }
+    /*Space optimised*/
+    public static int tabulation(int nums[],int k){
+         
+         int prev[]=new int[k+1];
+		 int curr[]=new int[k+1];
+        for(int i=0;i<=nums.length;i++)
+            prev[0]=1;
+        if(nums[0]<=k)
+        prev[nums[0]]=1;
+        for(int i=1;i<=nums.length;i++)
+        {
+            for(int j=1;j<=k;j++)
+            {
+               int exclude=prev[j];
+               int include=(nums[i-1]<=j)?prev[j-nums[i-1]]:0;
+               curr[j]=exclude|include;
+            }
+			prev=curr;
+        }
+        return prev[k];
+    }
+    
     static boolean isSubsetSum(int N, int arr[], int sum){
          int dp[][]=new int[arr.length+1][sum+1];
         for(int i=0;i<arr.length+1;i++)//sum=0
