228dd35a8c7e26788a1b4a41bff43e0502228d10
Adhisikha Patnaik <56517351+adhi-2311@users.noreply.github.com>
2024-06-19 21:20:38 +0530

    Update Inorder Succesor of BST.java

--- Trees/Inorder Succesor of BST.java
+++ Trees/Inorder Succesor of BST.java
@@ -4,63 +4,28 @@ Inorder Successor of an input node can also be defined as the node with the smal
 */
 class Solution
 {
-    public Node findMin(Node root)
-    {
-        if(root==null)
-        return null;
-        while(root.left!=null)
-        root=root.left;
-        
-        return root;
-    }
-    // returns the inorder successor of the Node x in BST (rooted at 'root')
-	public Node inorderSuccessor(Node root,Node x)
-    {
-          //add code here.
-          
-       if(x==null||root==null)
-       return null;
-       
-       //right subtree exists
-       if(x.right!=null)
+    public static void findPre(Node root,Node[] pre,int key){
+       while(root!=null)
        {
-           //find min value in right subtree
-           return findMin(x.right);
-       }
-       //right subtree is null
-       else
-       {
-          /*
-          10
-         / 
-        7
-       / \
-      4   8
-           
-          x=4.root=10=>ans=7
-          Here x is present in left of parent node(7) ,then inorder succesor=parent
-          We traverse from root till we hit the target node x and then return the parent
-          
-          x=8,root=10 =>ans=10
-          if x is present in right of parent node(7),then (left->parent->right so left subteee and parent both are visited)
-          we travel right of root till we hit the target node
-          
-          */
-           Node ancestor=root;
-           Node succesor=null;
-           while(ancestor!=null && ancestor!=succesor)
-           {
-             if(x.data<ancestor.data)
-             {
-               succesor=ancestor;
-               ancestor=ancestor.left;
-             }
-             else
-             ancestor=ancestor.right;
+           if(root.data>=key)
+           root=root.left;
+           else{
+               pre[0]=root;
+               root=root.right;
            }
-           return succesor;
        }
-      
-          
+       
+    }
+     public static void findSuc(Node root,Node[] suc,int key){
+       while(root!=null)
+       {
+           if(root.data<=key)
+           root=root.right;
+           else{
+               suc[0]=root;
+               root=root.left;
+           }
+       }
+       
     }
 }
